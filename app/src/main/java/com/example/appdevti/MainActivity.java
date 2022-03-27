e088cbf29fe5a30759350ade0868151652d48f27
Ben  Okello <mwakaben@gmail.com>
2022-03-27 21:01:19 +0300

    Activitty view details

--- app/src/main/java/com/example/appdevti/MainActivity.java
+++ app/src/main/java/com/example/appdevti/MainActivity.java
@@ -5,21 +5,54 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+ EditText feeIdINput;
+ Button viewData;
+ String _feeIdINput;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+            feeIdINput=findViewById(R.id.etFeeGroupId);
+            viewData=findViewById(R.id.btnViewData);
+
+            viewData.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    submitFeeId();
+                }
+            });
+
+    }
+// submit details
+    private void submitFeeId() {
+        _feeIdINput=feeIdINput.getText().toString().trim();
+
+        //Validation
+        if (_feeIdINput.isEmpty()){
+         //notification
+            Toast.makeText(this, "Please Input a Fee Id", Toast.LENGTH_SHORT).show();
+        }else {
+            Intent intent = new Intent(MainActivity.this,feeItemDetails.class);
+            //shared
+            intent.putExtra("feeIdInput",_feeIdINput);
+            startActivity(intent);
+        }
     }
 
     public void ClearStorageAction(View view) {
-        Intent intent = new Intent  (MainActivity.this,feeGroupsRecyclerViewActivity.class);
-        startActivity(intent);
+        ///Code to Clear local Storage
     }
 
+
     public void ViewStorageAction(View view) {
+        Intent intent = new Intent  (MainActivity.this,feeGroupsRecyclerViewActivity.class);
+        startActivity(intent);
     }
 
     public void ViewDataAction(View view) {
